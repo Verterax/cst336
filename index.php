1cf71c12c402b8ccc129adc0c3e5bfa22895e6f2
Christopher Caldwell <ccaldwell@csumb.edu>
2018-10-23 22:48:17 +0000

    initial

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title> CST-336 Assignment Page</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+        <h1>Labs</h1>
+    <ul style="list-style-type: none;">
+      <li><a href="lab1/index.html"><h2>Lab 1 (Personal Portfolio)</h2></a></li>
+    </ul>
+    
+    <h1>Homework</h1>
+    <ul style="list-style-type: none;">
+      <!--<li><a href="hw1/index.html"><h1>Homework 1</h2></a></li>-->
+    </ul>
+    
+    <!--<a href="teamproject/index.php"><button class="btn btn-success btn-lg">TeamProject</button></a><br />-->
+    <!--<a href="finalproject/index.php"><button class="btn btn-primary btn-lg">Final Project</button></a>-->
+    </body>
+</html>
