831248335baf743199257719808cbd1a37ef2e2f
Christopher Caldwell <ccaldwell@csumb.edu>
2018-10-24 02:32:19 +0000

    HW1 initial

--- index.php
+++ index.php
@@ -3,21 +3,96 @@
     <head>
         <meta charset="utf-8"/>
         <title> CST-336 Assignment Page</title>
-        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="mainStyles.css" rel="stylesheet" type="text/css" /
     </head>
 
     <body>
-        <h1>Labs</h1>
-    <ul style="list-style-type: none;">
-      <li><a href="lab1/index.html"><h2>Lab 1 (Personal Portfolio)</h2></a></li>
-    </ul>
+        <div id="table-header">
+            Chris Caldwell's CST-336 Assignments
+        </div>
+        <div id="assignments">
+            <table>
+                <tr class="assign-header">
+                    <td>Lab N</td>
+                    <td>Desc.</td>
+                </tr>
+                <!--Labs go below-->
+                <tr class= "assign-item">
+                    <td><a href="lab1/index.html">Lab 1</a></td>
+                    <td>Personal Portfolio</td>
+                </tr>
+                <tr class= "assign-item">
+                    <td><a href="lab2/index.html">Lab 2</a></td>
+                    <td>--</td>
+                </tr>
+                <tr class= "assign-item">
+                    <td><a href="lab3/index.html">Lab 3</a></td>
+                    <td>--</td>
+                </tr>
+                <tr class= "assign-item">
+                    <td><a href="lab4/index.html">Lab 4</a></td>
+                    <td>--</td>
+                </tr>
+                <tr class= "assign-item">
+                    <td><a href="lab5/index.html">Lab 5</a></td>
+                    <td>--</td>
+                </tr>
+                <tr class= "assign-item">
+                    <td><a href="lab6/index.html">Lab 6</a></td>
+                    <td>--</td>
+                </tr>
+                <tr class= "assign-item">
+                    <td><a href="lab7/index.html">Lab 7</a></td>
+                    <td>--</td>
+                </tr>
+                <tr class= "assign-item">
+                    <td><a href="lab8/index.html">Lab 8</a></td>
+                    <td>--</td>
+                </tr>
+                <tr class= "assign-item">
+                    <td><a href="lab9/index.html">Lab 9</a></td>
+                    <td>--</td>
+                </tr>
+                <tr class= "assign-item">
+                    <td><a href="lab10/index.html">Lab 10</a></td>
+                    <td>--</td>
+                </tr>
+                
+                <tr class="assign-header">
+                    <td>Homework N</td>
+                    <td>Desc.</td>
+                </tr>
+            
+                <!--Labs go below-->
+                <tr class= "assign-item">
+                    <td><a href="hw1/index.html">Homework 1</a></td>
+                    <td>CS Related Topic</td>
+                </tr>
+                <tr class= "assign-item">
+                    <td><a href="hw2/index.html">Homework 2</a></td>
+                    <td>--</td>
+                </tr>
+                <tr class= "assign-item">
+                    <td><a href="hw3/index.html">Homework 3</a></td>
+                    <td>--</td>
+                </tr>
+                <tr class= "assign-item">
+                    <td><a href="hw4/index.html">Homework 4</a></td>
+                    <td>--</td>
+                </tr>
+                
+                
+                <tr class="assign-header">
+                    <td>Team Project</td>
+                    <td>--</td>
+                </tr>
+                <tr class="assign-header">
+                    <td>Team Project</td>
+                    <td>--</td>
+                </tr>
+            </table>
+        </div>
+        
     
-    <h1>Homework</h1>
-    <ul style="list-style-type: none;">
-      <!--<li><a href="hw1/index.html"><h1>Homework 1</h2></a></li>-->
-    </ul>
-    
-    <!--<a href="teamproject/index.php"><button class="btn btn-success btn-lg">TeamProject</button></a><br />-->
-    <!--<a href="finalproject/index.php"><button class="btn btn-primary btn-lg">Final Project</button></a>-->
     </body>
 </html>
