b4ab879e1ae13ca56179f76b2bc1a6f6b9cd020d
Christopher Caldwell <ccaldwell@csumb.edu>
2018-10-26 22:36:44 +0000

    slots complete

---
+++ lab2/inc/functions.php
@@ -0,0 +1,51 @@
+<?php
+            
+            function play() {
+                for ($i=1; $i<4; $i++){
+                 ${"randVal" . $i} = rand(0,3);
+                    displaySymbol(${"randVal" . $i}, $i);
+                }
+                displayPoints($randVal1, $randVal2, $randVal3);
+            }
+
+            function displaySymbol($randomValue, $pos){
+                switch($randomValue){
+                    case 0:
+                        $symbol = "seven";
+                        break;
+                    case 1:
+                        $symbol = "cherry";
+                        break;
+                    case 2:
+                        $symbol = "lemon";
+                        break;
+                    case 3:
+                        $symbol = "bar";
+                }
+                echo "<img id='reel$pos'src='img/$symbol.png' alt='$symbol' title='" . ucfirst($symbol). "' width='70' />" ;
+            }
+
+            function displayPoints($randVal1, $randVal2, $randVal3){
+                echo "<div id='output'>";
+                if ($randVal1 == $randVal2 && $randVal2 == $randVal3) {
+                    switch ($randVal1){
+                        case 0: $totalPoints = 1000;
+                            echo "<h1>Jackpot!</h1>";
+                            
+                            echo "<audio autoplay> <source src='/cst336/lab2/audio/winning.mp3' type='audio/mpeg'> </audio>";
+                            break;
+                        case 1: $totalPoints = 500;
+                            break;
+                        case 2: $totalPoints = 250;
+                            break;
+                        case 3: $totalPoints = 900;
+                            break;
+                    }
+                    
+                    echo "<h2>You won $totalPoints points!!</h2>";
+                } else {
+                    echo "<h3>Try Again!</h3>";
+                }
+                echo "</div>";
+            }    
+?>
--- lab2/index.php
+++ lab2/index.php
@@ -1,3 +1,5 @@
+<?php include 'inc/functions.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,37 +7,14 @@
         <link rel="stylesheet" href="css/styles.css">
     </head>
     <body>
+        <div id="main">
+            <?php 
+                play();
+            ?>
         
-        <?php 
-            for ($i=0; $i<3; $i++){
-                $val = rand(0,2);
-                displaySymbol($val);
-            }
-
-            
-            function displaySymbol($randomValue){
-            //     if ($randomValue == 0){
-            //         echo '<img src="img/seven.png" alt="seven" title="Seven" width="70 />"<br>';
-            //     } else if ($randomValue == 1) {
-            //         echo '<img src="img/cherry.png" alt="cherry" title="Cherry" width="70 />"<br>';
-            //     } else {
-            //         echo '<img src="img/lemon.png" alt="lemon" title="Lemon" width="70 />"<br>' ;
-            //     }
-                
-                switch($randomValue){
-                    case 0:
-                        $symbol = "seven";
-                        break;
-                    case 1:
-                        $symbol = "cherry";
-                        break;
-                    case 2:
-                        $symbol = "lemon";
-                        break;
-                }
-                echo "<img src='img/$symbol.png' alt='$symbol' title='" . ucfirst($symbol). "' width='70' />" ;
-                
-            }
-        ?>
+            <form>
+                <input type="submit" value="Spin!"/>
+            </form>
+        </div>
     </body>
 </html>
