174ee9bffe8fe6ef98492710dfa7565feb76076f
Christopher Caldwell <ccaldwell@csumb.edu>
2018-10-28 04:59:18 +0000

    Lab2 progress

--- hw2/inc/functions.php
+++ hw2/inc/functions.php
@@ -1,33 +1,42 @@
 <?php
-    function cardTest()
+    
+    function newGame() 
     {
-        echo "<table>";
-        for ($tr = 0; $tr < 3; $tr++)
-        {
-            echo "<tr>";
-            for ($tc = 0; $tc < 6; $tc++)
-            {
-                echo "<td>";
-                echo "<img onmouseover='playHoverSound()' onclick='playClickSound()' class='card cardback' src='img/cardback.png'/>";
-                echo "</td>";
-            }
-            echo "</tr>";
-        }
-        echo "</table>";
+        $cookieName = "Mario3Babyyyyyy!!";
+        $cards = initCards();
+        $saveCardsCookie = implode(",", $cards);
+        setcookie($cookieName, $saveCardsCookie, time() + 84600, "/");
+        return $cards;
     }
     
-    function getArgument(){
-        $arg = $_GET['tableIndex'];
-        if (isset($arg))
-        {
-            return $arg;
-        }
+    function loadCards() 
+    {
+        $cookieName = "Mario3Babyyyyyy!!";
+        if(!isset($_COOKIE[$cookieName]))
+            return newGame();
         else
+            return explode(",", $_COOKIE[$cookieName]);
+    }
+    
+    function saveCards($currentCards)
+    {
+        $cookieName = "Mario3Babyyyyyy!!";
+        $saveCardsCookie = implode(",", $currentCards);
+        setcookie($cookieName, $saveCardsCookie, time() + 84600, "/");
+    }
+    
+    function initCards() 
+    {
+        $cards = array(-1,-1,-2,-2,-3,-3,-4,-4,-4,-4,-5,-5,-5,-5,-6,-6,-6,-6);
+        for ($i = 0; $i < 1000; $i++)
         {
-            return null;
+            $indexA = mt_rand(0,17);
+            $indexB = mt_rand(0,17);
+            $temp = $cards[$indexA];
+            $cards[$indexA] = $cards[$indexB];
+            $cards[$indexB] = $temp;
         }
-            
-        
+        return $cards;
     }
     
     // Check for matches and mismatches
@@ -37,51 +46,34 @@
         if ($cards[$tableIndex] > 0)
             return null;
             
-        // Make it positive, and mark it x11.
+        // Make it positive, and mark it times neg 11.
+        // It was neg, neg x neg = pos N*11.
+        // It has been marked to either be a match, or a mismatch
         $cards[$tableIndex] *= -11;
-    
-        //Return the sound to play, if any.
-        for ($i = 0; $i < 18; $i++)
-        {
-            if ($cards[$i] % 11 == 0 && $i != $tableIndex)
-            {
-                // Two cards are flipped. Do they match?
-                if ($cards[$i] == $cards[$tableIndex])
-                {
-                    // Match (Set to face value by dividing by 11.)
-                    $cardId[$i] /= 11;
-                    $cardId[$tableIndex] /= 11;
-                    
-                    if ($cardId[$i] == 1)
-                    {
-                        return "oneup";
-                    }
-                    return "match";
-                        
-                }
-                else // No match.
-                {
-                    return "mismatch";
-                }
-            }
-        }
-        
-        return "flip";
     }
     
-    function displayCards($showAll) 
+    function forceWin(&$cards){
+        for ($i = 0; $i < 18; $i++)
+        {
+            if ($cards[$i] < 0)
+                $cards[$i] *= -1;
+            if ($cards[$i] % 11 == 0)
+                $cards[$i] /= 11;
+        }
+    }
+    
+    function displayCards() 
     {
+        //newgame();
         $cards = loadCards();
         $arg = getArgument();
-        
+
         if ($arg != null)
         {
-            $result = mutate($cards, $arg);
-            if ($result != null)
-            {
-                saveCards($cards);
-            }
+            mutate($cards, $arg);
         }
+        
+        //forceWin($cards);
         
         $tableIndex = 0;
         echo "<table>";
@@ -93,11 +85,6 @@
                 echo "<td>";
                 
                 $cardId = $cards[$tableIndex];
-                if ($showAll)
-                {
-                    $cardId = abs($cardId);
-                }
-                
                 $cardName = getCardName($cardId);
                 
                 if ($cardId < 0)
@@ -117,53 +104,67 @@
         }
         echo "</table>";
         
+        // Execute post results.
         // Play Result Audio
-        // printCards($cards);
-        
+        $match = checkMatch($cards);
+        if($match < 0)
+        {
+            echo "<script>window.onload = function(){playMismatch();};</script>";
+            flipBack($cards);
+            saveCards($cards);
+        }
+        elseif ($match > 0)
+        {
+            // Match
+            $matchCode = getMatchCode($cards);
+            echo "<script>window.onload = function(){playMatch('$matchCode');};</script>";
+            keepFlipped($cards);
+            saveCards($cards);
+        }
+        else // Do nothing, just one card is flipped.
+        {
+            saveCards($cards);
+        }
+
+        if (didWin($cards))
+        {
+            echo "<script>window.onload = function(){playWin();};</script>";
+            newgame();
+        }
     }
     
-    function loadCards() 
-    {
-        $cookieName = "Mario3Babyyyyyy!!";
-        if(!isset($_COOKIE[$cookieName]))
-        {
-            return newGame();
-        }
+    function getArgument(){
+        $arg = $_GET['tableIndex'];
+        if (isset($arg))
+            return $arg;
         else
-        {
-            return explode(",", $_COOKIE[$cookieName]);
-        }
+            return null;
     }
     
-    function saveCards($currentCards)
+    //Flip back over mismatched cards
+    function flipBack(&$cards)
     {
-        $cookieName = "Mario3Babyyyyyy!!";
-        $saveCardsCookie = implode(",", $currentCards);
-        setcookie($cookieName, $saveCardsCookie, time() + 84600, "/");
+        for ($i = 0; $i < 18; $i++)
+            if ($cards[$i] % 11 == 0)
+                $cards[$i] /= -11;
     }
     
-    function newGame() 
+    function keepFlipped(&$cards)
     {
-        $cookieName = "Mario3Babyyyyyy!!";
-        $cards = initCards();
-        $saveCardsCookie = implode(",", $cards);
-        setcookie($cookieName, $saveCardsCookie, time() + 84600, "/");
-        return $cards;
+        for ($i = 0; $i < 18; $i++)
+            if ($cards[$i] % 11 == 0)
+                $cards[$i] /= 11;
     }
     
-    function initCards() 
+    function getMatchCode($cards)
     {
-        $cards = array(-1,-1,-2,-2,-3,-3,-4,-4,-4,-4,-5,-5,-5,-5,-6,-6,-6,-6);
-        for ($i = 0; $i < 1000; $i++)
-        {
-            $indexA = mt_rand(0,17);
-            $indexB = mt_rand(0,17);
-            $temp = $cards[$indexA];
-            $cards[$indexA] = $cards[$indexB];
-            $cards[$indexB] = $temp;
-        }
-        return $cards;
+        for ($i = 0; $i < 18; $i++)
+            if($cards[$i] % 11 == 0)
+                return getCardName($cards[$i]);
+
+        return null;
     }
+    
     
     function getCardName($cardId)
     {
@@ -192,9 +193,34 @@
         }
     }
     
-    function printCards($input) {
-        print_r($input);
+    function checkMatch($cards)
+    {
+        $count = 0;
+        $compareCards = array();
+        for ($i = 0; $i < 18; $i++)
+        {
+            if ($cards[$i] % 11 == 0)
+            {
+                array_push($compareCards, $cards[$i]);
+                $count = count($compareCards);
+            }
+        }
+        
+        if ($count == 2 && $compareCards[0] == $compareCards[1])
+            return 1;
+        elseif ($count >= 2)
+            return -1;
+        else
+            return 0;
     }
     
+    function didWin($cards)
+    {
+        for ($i = 0; $i < 18; $i++)
+            if ($cards[$i] < 0)
+                return false;
+        
+        return true;
+    }
 
 ?>
--- hw2/index.php
+++ hw2/index.php
@@ -4,31 +4,50 @@
     <head>
         <title> Mario 3 Card Game</title>
         <link rel="stylesheet" href="css/styles.css">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
         <audio id="hoverSound"> <source src="audio/select.mp3" type="audio/mpeg"> </audio>
         <audio id="cardFlip"> <source src="audio/flip.mp3" type="audio/mpeg">" </audio>
+        <audio id="mismatch"> <source src="audio/mismatch.mp3" type="audio/mpeg">" </audio>
+        <audio id="match"> <source src="audio/match.mp3" type="audio/mpeg">" </audio>
+        <audio id="oneup"> <source src="audio/oneup.mp3" type="audio/mpeg">" </audio>
+        <audio id="win"> <source src="audio/win.mp3" type="audio/mpeg">" </audio>
     </head>
-    <body>
+    <!--<body>-->
+    <body onload="playMusic()">
         <div id="bg-stripes">
             <!--Music-->
-            <audio autoplay loop>
-                <source src="audio/songloop2.mp3" type="audio/mpeg">
-            </audio>
+            <!--<audio autoplay loop>-->
+            <!--    <source src="audio/songloop2.mp3" type="audio/mpeg">-->
+            <!--</audio>-->
             
             <div id="play-space">
                 <?php 
-                    displayCards(false); 
+                    displayCards(); 
                 ?>
             </div>
         </div>
         <img id="play-bar" src="img/bottom_bar.png" alt=""/>
-        <?php 
-            
-        ?>
+        
+        <footer>
+            <div>
+            <hr>
+            CST-336. 2018 &copy; Caldwell <br/>
+            <strong>Disclaimer:</strong> The information in this webpage is used for academic purposes only.<br/>
+            <img src="img/csumb_logo.png" alt="CSUMB Otter Logo" />
+            </div>
+        </footer>
     </body>
+    
+
     
     <script>
         var hoverSound = document.getElementById("hoverSound");
         var clickSound = document.getElementById("cardFlip");
+        var mismatch = document.getElementById("mismatch");
+        var match = document.getElementById("match");
+        var oneup = document.getElementById("oneup");
+        var win = document.getElementById("win");
+        var music = new Audio("audio/songloop2.mp3");
         function playHoverSound() {
             hoverSound.play();
         }
@@ -37,8 +56,28 @@
             setTimeout(function() { redirect(tableIndex); }, 450);
         }
         function redirect(tableIndex) {
-            window.location.href = 'index.php?tableIndex=' + tableIndex;
+            if (typeof tableIndex === "undefined")
+                window.location.href = 'index.php';
+            else
+                window.location.href = 'index.php?tableIndex=' + tableIndex;
+        }
+        function playMismatch() {
+            mismatch.play();
+            setTimeout(function() { redirect(); }, 800);
+        }
+        function playMatch(cardName) {
+            if (cardName == "oneup")
+                oneup.play();
+            else
+                match.play();
+        }
+        function playWin() {
+            win.play();
+            setTimeout(function() { redirect(); }, 4000);
+        }
+        function playMusic(){
+            music.loop = true;
+            music.play();
         }
     </script>
-    
 </html>
